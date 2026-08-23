68b31353f21fbb0918259ca2ae364acda0827c56
Himanshu Pawar <himanshu@Himanshus-MacBook-Air.local>
2026-08-23 17:00:30 +0530

    three step

--- pipeline.groovy
+++ pipeline.groovy
@@ -15,7 +15,11 @@ pipeline {
         
         stage ("test"){
             steps {
-                echo 'build success'            
+               sh ''' sudo -u jenkins /opt/apache-maven-3.9.16/bin/mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar   
+                     -Dsonar.projectKey=pipeline \ 
+                     -Dsonar.projectName='pipeline' \   
+                     -Dsonar.host.url=http://54.198.6.54:9000 \   
+                     -Dsonar.token=sqp_efc9f9390bb0530130ea5d8ac1f8bfca729aadc0'''            
                 
             }
         }
