b8a8c95b3f98d1ed5975b22f3e5e27022f641d9a
Himanshu Pawar <himanshu@Himanshus-MacBook-Air.local>
2026-08-23 17:08:13 +0530

    new

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,30 +1,33 @@
 pipeline {
     agent any
+
     stages {
-        stage ("pull"){
+
+        stage("pull") {
             steps {
-                git 'https://github.com/cloudmaster2025/sonar.git'            
+                git 'https://github.com/cloudmaster2025/sonar.git'
             }
         }
-        
-        stage ("build "){
+
+        stage("build") {
             steps {
-                  sh '/opt/apache-maven-3.9.16/bin/mvn clean package'         
-                }
-        }
-        
-        stage ("test"){
-            steps {
-               sh ''' sudo -u jenkins /opt/apache-maven-3.9.16/bin/mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
-                     -Dsonar.projectKey=pipeline \
-                     -Dsonar.projectName='pipeline' \
-                     -Dsonar.host.url=http://54.198.6.54:9000 \  
-                     -Dsonar.token=sqp_efc9f9390bb0530130ea5d8ac1f8bfca729aadc0'''            
-                
+                sh '/opt/apache-maven-3.9.16/bin/mvn clean package'
             }
         }
-    
-        stage ("deploy") {
+
+        stage("test") {
+            steps {
+                sh '''
+                    /opt/apache-maven-3.9.16/bin/mvn clean verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar \
+                    -Dsonar.projectKey=pipeline \
+                    -Dsonar.projectName=pipeline \
+                    -Dsonar.host.url=http://54.198.6.54:9000 \
+                    -Dsonar.token=sqp_efc9f9390bb0530130ea5d8ac1f8bfca729aadc0
+                '''
+            }
+        }
+
+        stage("deploy") {
             steps {
                 echo 'deployment complete'
             }
